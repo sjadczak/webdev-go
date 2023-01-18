2e020bd1655763cc9c87bdf1c7bd9c0bf4b19278
Steve Jadczak <stevejadczak@gmail.com>
2023-01-17 22:17:59 -0500

    bad .gitignore --fixed

---
+++ lenslocked/views/template.go
@@ -0,0 +1,47 @@
+package views
+
+import (
+	"fmt"
+	"html/template"
+	"io/fs"
+	"log"
+	"net/http"
+)
+
+type Template struct {
+	htmlTmpl *template.Template
+}
+
+func Must(t Template, err error) Template {
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
+func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
+	tpl, err := template.ParseFS(fs, patterns...)
+	if err != nil {
+		return Template{}, fmt.Errorf("parsing template: %w", err)
+	}
+
+	return Template{tpl}, nil
+}
+
+func Parse(path string) (Template, error) {
+	tpl, err := template.ParseFiles(path)
+	if err != nil {
+		return Template{}, fmt.Errorf("parsing template: %w", err)
+	}
+
+	return Template{tpl}, nil
+}
+
+func (t Template) Execute(w http.ResponseWriter, data interface{}) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	err := t.htmlTmpl.Execute(w, data)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
+	}
+}
