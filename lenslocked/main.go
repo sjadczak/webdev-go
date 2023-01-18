e61fb82d3e1bcee33cec8cf0bf609ac976b7d091
Steve Jadczak <stevejadczak@gmail.com>
2023-01-17 21:30:13 -0500

    embedded templates in binary & added support for parsing from FS

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -3,26 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/sjadczak/webdev-go/lenslocked/controllers"
+	"github.com/sjadczak/webdev-go/lenslocked/templates"
 	"github.com/sjadczak/webdev-go/lenslocked/views"
 )
 
 func main() {
 	r := chi.NewRouter()
 
-	tpl := views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))
+	tpl := views.Must(views.ParseFS(templates.FS, "home.gohtml"))
 	r.Get("/", controllers.StaticHandler(tpl))
 
-	tpl = views.Must(views.Parse(filepath.Join("templates", "contact.gohtml")))
+	tpl = views.Must(views.ParseFS(templates.FS, "contact.gohtml"))
 	r.Get("/contact", controllers.StaticHandler(tpl))
 
-	tpl = views.Must(views.Parse(filepath.Join("templates", "faq.gohtml")))
+	tpl = views.Must(views.ParseFS(templates.FS, "faq.gohtml"))
 	r.Get("/faq", controllers.StaticHandler(tpl))
 
-	tpl = views.Must(views.Parse(filepath.Join("templates", "notfound.gohtml")))
+	tpl = views.Must(views.ParseFS(templates.FS, "notfound.gohtml"))
 	r.NotFound(controllers.StaticHandler(tpl))
 
 	fmt.Println("Starting the server on :3000...")
