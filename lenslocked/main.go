af6d5d59564e3abcf25e8eb60c3ff8a76771a73d
Steve Jadczak <stevejadczak@gmail.com>
2023-01-17 20:47:02 -0500

    Using StaticHandler

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -6,55 +6,25 @@ import (
 	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/sjadczak/webdev-go/lenslocked/controllers"
 	"github.com/sjadczak/webdev-go/lenslocked/views"
 )
 
-func executeTemplate(w http.ResponseWriter, filepath string) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tpl, err := views.Parse(filepath)
-	if err != nil {
-		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
-		return
-	}
-	tpl.Execute(w, nil)
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "home.gohtml")
-	executeTemplate(w, tplPath)
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "contact.gohtml")
-	executeTemplate(w, tplPath)
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "faq.gohtml")
-	executeTemplate(w, tplPath)
-}
-
-func notFoundHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "notfound.gohtml")
-	executeTemplate(w, tplPath)
-}
-
-func configureRouter() chi.Router {
-	// Allocate new mux
+func main() {
 	r := chi.NewRouter()
 
-	// Add routes
-	r.Get("/", indexHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
-	r.NotFound(notFoundHandler)
+	tpl := views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))
+	r.Get("/", controllers.StaticHandler(tpl))
 
-	return r
-}
+	tpl = views.Must(views.Parse(filepath.Join("templates", "contact.gohtml")))
+	r.Get("/contact", controllers.StaticHandler(tpl))
 
-func main() {
-	router := configureRouter()
+	tpl = views.Must(views.Parse(filepath.Join("templates", "faq.gohtml")))
+	r.Get("/faq", controllers.StaticHandler(tpl))
+
+	tpl = views.Must(views.Parse(filepath.Join("templates", "notfound.gohtml")))
+	r.NotFound(controllers.StaticHandler(tpl))
 
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe("127.0.0.1:3000", router)
+	http.ListenAndServe("127.0.0.1:3000", r)
 }
