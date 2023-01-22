3372439c60a8b4109aefddd2f59911f9734a007c
Steve Jadczak <stevejadczak@gmail.com>
2023-01-21 23:27:31 -0500

    s8 complete

--- lenslocked/controllers/users.go
+++ lenslocked/controllers/users.go
@@ -20,17 +20,6 @@ func (u Users) New(w http.ResponseWriter, r *http.Request) {
 }
 
 func (u Users) Create(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseMultipartForm(4000 << 10)
-	if err != nil {
-		panic(err)
-	}
-	f, handler, err := r.FormFile("file")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
 	fmt.Fprintf(w, "Email:    %v\n", r.FormValue("email"))
 	fmt.Fprintf(w, "Password: %v\n", r.FormValue("password"))
-	fmt.Fprintf(w, "Filename: %v\n", handler.Filename)
 }
