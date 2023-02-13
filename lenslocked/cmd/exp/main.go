f22496214ed04f13f43296b4b2e7740130423094
Jadczak, Stephen [JRDUS] <SJadczak@its.jnj.com>
2023-02-13 16:18:30 -0500

    built mini-cli to play with bcrypt

--- lenslocked/cmd/exp/main.go
+++ lenslocked/cmd/exp/main.go
@@ -25,7 +25,7 @@ func (cfg PostgresConfig) String() string {
 
 func main() {
 	cfg := PostgresConfig{
-		Host:     "localhost",
+		Host:     "192.168.99.102",
 		Port:     "5432",
 		User:     "dev",
 		Password: "dev",
@@ -69,18 +69,79 @@ func main() {
 	fmt.Println("lenslocked> tables created.")
 
 	// Insert some data
-	name := "Jessica Jadczak"
-	email := "jessica@test.com"
+	//name := "Jessica Jadczak"
+	//email := "jessica@test.com"
+	//row := db.QueryRow(`
+	//INSERT INTO users (name, email)
+	//VALUES ($1, $2)
+	//RETURNING id;
+	//`, name, email)
+	//var id int
+	//err = row.Scan(&id)
+	//if err != nil {
+	//panic(err)
+	//}
+	//fmt.Printf("lenslocked> user created with id %d.\n")
+
+	id := 1
 	row := db.QueryRow(`
-		INSERT INTO users (name, email)
-		VALUES ($1, $2)
-		RETURNING id;
-	`, name, email)
-	var id int
-	err = row.Scan(&id)
+		SELECT name, email
+		FROM users
+		WHERE id=$1;
+	`, id)
+	var name, email string
+	err = row.Scan(&name, &email)
 	if err != nil {
 		panic(err)
 	}
+	fmt.Printf("lenslocked> User Info: name=%s email=%s\n", name, email)
 
-	fmt.Printf("lenslocked> user created with id %d.\n", id)
+	//userID := 3
+	//for i := 1; i <= 6; i++ {
+	//amount := i * 20
+	//desc := fmt.Sprintf("Fake order #%d", i+5)
+	//_, err := db.Exec(`
+	//INSERT INTO orders(user_id, amount, description)
+	//VALUES ($1, $2, $3);
+	//`, userID, amount, desc)
+	//if err != nil {
+	//panic(err)
+	//}
+	//}
+	//fmt.Println("lenslocked> created faker orders.")
+
+	type Order struct {
+		ID          int
+		UserID      int
+		Amount      int
+		Description string
+	}
+	var orders []Order
+
+	userID := 1
+	rows, err := db.Query(`
+		SELECT id, amount, description
+		FROM orders
+		WHERE user_id=$1
+	`, userID)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var order Order
+		order.UserID = userID
+		err = rows.Scan(&order.ID, &order.Amount, &order.Description)
+		if err != nil {
+			panic(err)
+		}
+		orders = append(orders, order)
+	}
+	if err = rows.Err(); err != nil {
+		panic(err)
+	}
+	fmt.Printf("lenslocked> found %d orders for user %d.\n", len(orders), userID)
+	for _, order := range orders {
+		fmt.Printf("#> Order %d: Quantity=%d Description=%s\n", order.ID, order.Amount, order.Description)
+	}
 }
--- lenslocked/go.mod
+++ lenslocked/go.mod
@@ -2,7 +2,10 @@ module github.com/sjadczak/webdev-go/lenslocked
 
 go 1.19
 
-require github.com/go-chi/chi/v5 v5.0.8
+require (
+	github.com/go-chi/chi/v5 v5.0.8
+	github.com/jackc/pgx/v4 v4.17.2
+)
 
 require (
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
@@ -12,7 +15,7 @@ require (
 	github.com/jackc/pgproto3/v2 v2.3.1 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	github.com/jackc/pgtype v1.12.0 // indirect
-	github.com/jackc/pgx/v4 v4.17.2 // indirect
-	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
-	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
 )
