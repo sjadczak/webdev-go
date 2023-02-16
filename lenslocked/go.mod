d4ee5dc8354fba0e49cb8abafb5cac8a70783d5d
Steve Jadczak <stevejadczak@gmail.com>
2023-02-15 22:01:43 -0500

    complete through CSRF middleware

--- lenslocked/go.mod
+++ lenslocked/go.mod
@@ -8,6 +8,8 @@ require (
 )
 
 require (
+	github.com/gorilla/csrf v1.7.1 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.13.0 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -15,6 +17,7 @@ require (
 	github.com/jackc/pgproto3/v2 v2.3.1 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	github.com/jackc/pgtype v1.12.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
--- lenslocked/main.go
+++ lenslocked/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/gorilla/csrf"
 	"github.com/sjadczak/webdev-go/lenslocked/controllers"
 	"github.com/sjadczak/webdev-go/lenslocked/models"
 	"github.com/sjadczak/webdev-go/lenslocked/templates"
@@ -42,10 +43,25 @@ func main() {
 	r.Post("/users", usersC.Create)
 	r.Get("/signin", usersC.SignIn)
 	r.Post("/signin", usersC.ProcessSignIn)
+	r.Get("/users/me", usersC.CurrentUser)
 
 	tpl = views.Must(views.ParseFS(templates.FS, "layout.gohtml", "notfound.gohtml"))
 	r.NotFound(controllers.StaticHandler(tpl))
 
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe("127.0.0.1:3000", r)
+	csrfKey := "a29fghadf092yh3rhglaisdfh2as$@Fas"
+	csrfMw := csrf.Protect(
+		[]byte(csrfKey),
+		// TODO: fix before deploying
+		csrf.Secure(false),
+	)
+	http.ListenAndServe("127.0.0.1:3000", csrfMw(r))
 }
+
+//func TimerMiddleware(h http.HandlerFunc) http.HandlerFunc {
+//return func(w http.ResponseWriter, r *http.Request) {
+//start := time.Now()
+//h(w, r)
+//fmt.Println("Request time:", time.Since(start))
+//}
+//}
