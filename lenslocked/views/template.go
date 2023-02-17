658e1a81340d66fd3197fc3e3e8d2cb9b11630b4
Steve Jadczak <stevejadczak@gmail.com>
2023-02-16 21:30:45 -0500

    csrf implemented

--- lenslocked/views/template.go
+++ lenslocked/views/template.go
@@ -1,11 +1,15 @@
 package views
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
+	"io"
 	"io/fs"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/csrf"
 )
 
 type Template struct {
@@ -20,7 +24,16 @@ func Must(t Template, err error) Template {
 }
 
 func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
-	tpl, err := template.ParseFS(fs, patterns...)
+	tpl := template.New(patterns[0])
+	tpl = tpl.Funcs(
+		template.FuncMap{
+			"csrfField": func() (template.HTML, error) {
+				return "", fmt.Errorf("csrfField not implemented.")
+			},
+		},
+	)
+
+	tpl, err := tpl.ParseFS(fs, patterns...)
 	if err != nil {
 		return Template{}, fmt.Errorf("parsing template: %w", err)
 	}
@@ -28,20 +41,38 @@ func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
 	return Template{tpl}, nil
 }
 
-func Parse(path string) (Template, error) {
-	tpl, err := template.ParseFiles(path)
+//func Parse(path string) (Template, error) {
+//tpl, err := template.ParseFiles(path)
+//if err != nil {
+//return Template{}, fmt.Errorf("parsing template: %w", err)
+//}
+
+//return Template{tpl}, nil
+//}
+
+func (t Template) Execute(w http.ResponseWriter, r *http.Request, data interface{}) {
+	tpl, err := t.htmlTmpl.Clone()
 	if err != nil {
-		return Template{}, fmt.Errorf("parsing template: %w", err)
+		log.Printf("cloning template: %v\n", err)
+		http.Error(w, "There was an error rendering the page.", http.StatusInternalServerError)
+		return
 	}
+	tpl = tpl.Funcs(
+		template.FuncMap{
+			"csrfField": func() template.HTML {
+				return csrf.TemplateField(r)
+			},
+		},
+	)
 
-	return Template{tpl}, nil
-}
-
-func (t Template) Execute(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	err := t.htmlTmpl.Execute(w, data)
+
+	var buf bytes.Buffer
+	err = tpl.Execute(&buf, data)
 	if err != nil {
 		log.Printf("executing template: %v", err)
 		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
 	}
+
+	io.Copy(w, &buf)
 }
