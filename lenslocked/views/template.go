7735d7fcf2b585ed7f81a36be1af861faa7bf060
Steve Jadczak <stevejadczak@gmail.com>
2023-04-26 22:04:53 -0400

    complete through alert js
    Signed-off-by: Steve Jadczak <stevejadczak@gmail.com>

--- lenslocked/views/template.go
+++ lenslocked/views/template.go
@@ -35,6 +35,13 @@ func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
 			"currentUser": func() (template.HTML, error) {
 				return "", fmt.Errorf("currentUser not implemented.")
 			},
+			"errors": func() []string {
+				return []string{
+					"Don't do that!",
+					"The email address you provided is already associated with an account.",
+					"Something went wrong",
+				}
+			},
 		},
 	)
 
